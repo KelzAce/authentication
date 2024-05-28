a161bc3561962f30916c0bcf35bc4acbeb48c34e
IK3CHI <ikechigreat@gmail.com>
2024-05-28 18:48:56 +0100

    auth system

---
+++ src/users/dto/user-signup.dto.ts
@@ -0,0 +1,8 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { UserSignInDto } from './user-signin.dto';
+
+export class UserSignUpDto extends UserSignInDto {
+  @IsNotEmpty({ message: 'Name cannot be null' })
+  @IsString({ message: 'Name must be a string' })
+  name: string;
+}
