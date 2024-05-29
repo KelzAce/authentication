246c85a8088efdb103719877eeac8645023deb9e
Okoro Ikechi Emmanuel <87893545+KelzAce@users.noreply.github.com>
2024-05-29 16:54:54 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CurrentUserMiddleware } from './utility/middlewares/current-user.middleware';
+import { UsersService } from './users/users.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1/')
+  app.setGlobalPrefix('api/v1');
+  const usersService = app.get(UsersService);
+  app.use(new CurrentUserMiddleware(usersService).use);
   await app.listen(3000);
 }
 bootstrap();
